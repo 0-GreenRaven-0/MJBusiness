c6b6a88238d4a6521f3076c59a2951e998f29a66
Matthew Hamdech <greenraven1000@gmail.com>
2025-11-06 16:36:06 +0200

    translated the new section

--- src/sections/Dropshipping.jsx
+++ src/sections/Dropshipping.jsx
@@ -14,7 +14,7 @@ const Dropshipping = () => {
         threshold={0.3}
         duration={0.5}
         retrigger={true}>
-        <h1 className="text-center font-bold text-white md:py-5 xl:py-0">What exactly is drop<span className="text-blue-300">shipping</span>?</h1>
+        <h1 className="text-center font-bold text-white md:py-5 xl:py-0">{t('dropshipping.title')}<span className="text-blue-300">{t('dropshipping.titleHighlight')}</span>?</h1>
       </AppearOnScroll>
       <div className="flex flex-col xl:flex-row xl:pt-10 ">
            <div className="xl:px-20">
@@ -26,7 +26,7 @@ const Dropshipping = () => {
           threshold={0.3}
           duration={0.5}
           retrigger={true}>
-          <p className="px-0">Dropshipping is an online business model where you sell different kinds of products through an online Shopify store, similar to e-commerce, except you don't have to manage inventory.</p>
+          <p className="px-0">{t('dropshipping.description')}</p>
         </AppearOnScroll>
        <div>
        <br/>
@@ -35,7 +35,7 @@ const Dropshipping = () => {
           threshold={0.3}
           duration={0.5}
           retrigger={true}>
-          <h2 className="md:text-4xl xl:text-2xl">How does it work?</h2>
+          <h2 className="md:text-4xl xl:text-2xl">{t('dropshipping.howItWorksTitle')}</h2>
         </AppearOnScroll>
         <br/>
           <ul className='flex flex-col items-start gap-5 text-white'>
@@ -78,7 +78,7 @@ const Dropshipping = () => {
           threshold={0.3}
           duration={0.5}
           retrigger={true}>
-          <p className="px-0">Based on this model, Majd was able to develop his dropshipping software that contains the tools, knowledge, strategies, and all his experience so you never have to start from scratch.</p>
+          <p className="px-0">{t('dropshipping.closing')}</p>
         </AppearOnScroll>
        </div>
       </div>
